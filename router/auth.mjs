e77cfeeb95475f3d02cdf8571e6762728550f855
태윤 <gmango777@gmail.com>
2025-11-21 12:34:10 +0900

    배열 데이터 api 추가 , user 추가

--- router/auth.mjs
+++ router/auth.mjs
@@ -3,8 +3,10 @@ import express, { Router } from "express";
 const router = express.Router();
 
 // 회원 가입
+router.post("/signup", authController.signup);
 
 // 로그인
+router.post("/login", authController.login);
 
 // 로그인 유지
 
