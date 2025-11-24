60dd8ec58ba2a94fb84542ccbbc1a1ce561e5eca
태윤 <gmango777@gmail.com>
2025-11-24 11:25:12 +0900

    Mysql 세팅

--- app.mjs
+++ app.mjs
@@ -2,6 +2,7 @@ import express from "express";
 import postsRouter from "./router/posts.mjs";
 import authRouter from "./router/auth.mjs";
 import { config } from "./config.mjs";
+import { db } from "./db/database.mjs";
 
 const app = express();
 
@@ -14,4 +15,5 @@ app.use((req, res, next) => {
   res.sendStatus(404);
 });
 
-app.listen(8080);
+db.getConnection().then((connection) => console.log(connection));
+app.listen(config.host.port);
