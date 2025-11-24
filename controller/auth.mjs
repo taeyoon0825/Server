cd5d3f12914aec98ea56d1f7e9e598c07dee9b79
태윤 <gmango777@gmail.com>
2025-11-24 09:41:53 +0900

    JWT 토큰 적용

--- controller/auth.mjs
+++ controller/auth.mjs
@@ -44,13 +44,15 @@ export async function login(req, res, next) {
 
   const token = await createJwtToken(user.id);
   res.status(200).json({ token, user });
-
-  if (user) {
-    res.status(200).json({ message: `${userid}님 로그인 완료!` });
-  } else {
-    res
-      .status(404)
-      .json({ message: `${userid}님 아이디 또는 비밀번호를 확인하세요` });
-  }
 }
+
 // 로그인 유지 함수
+
+export async function me(req, res, next) {
+  //   const user = await authRepository.findByUserid(req.id);
+  //   if (!user) {
+  //     return res.status(404).json({ message: "일치하는 사용자가 없습니다" });
+  //   }
+  //   res.status(200).json({ token: req.token, userid: user.userid });
+  res.status(200).json({ message: "성공했어~" });
+}
---
+++ middleware/auth.mjs
@@ -0,0 +1,35 @@
+import jwt, { decode } from "jsonwebtoken";
+import * as authRepository from "../data/auth.mjs";
+
+const AUTH_ERROR = { message: "인증 에러" };
+
+export const isAuth = async (req, resizeBy, next) => {
+  const authHeader = req.get("Authorization");
+  console.log(authHeader);
+
+  if (!(authHeader && authHeader.startsWith("Bearer "))) {
+    console.log("헤더 에러");
+    return resizeBy.status(401).json(AUTH_ERROR);
+  }
+
+  //Authorization: Bearer QEWDFADFEVSDSDTSDFVCE== 비어러부터시작한 전체가 헤더. 띄어쓰기 기반 1번은 토큰
+  const token = authHeader.split(" ")[1];
+  // console.log("토큰 분리 성공", token);
+
+  jwt.verify(token, "abcdefg1234!@#$", async (error, decoded) => {
+    if (error) {
+      console.log("토큰 에러");
+      return res.status(401).json(AUTH_ERROR);
+    }
+    console.log(decoded);
+    const user = await authRepository.findById(decoded.id);
+    if (!user) {
+      console.log("아이디 없음");
+      return res.status(401).json(AUTH_ERROR);
+    }
+    console.log("user.id: ", user.id);
+    console.log("user.userid: ", user.userid);
+    req.userid = user.userid;
+    next();
+  });
+};
