c35d852451ac35042f75b4ac71494e1efe16033e
태윤 <gmango777@gmail.com>
2025-11-24 10:24:16 +0900

    config 수정

--- config.mjs
+++ config.mjs
@@ -3,9 +3,9 @@ import dotenv from "dotenv";
 dotenv.config();
 
 function required(key, defaultValue = undefined) {
-  const value = process.env[key] | defaultValue;
+  const value = process.env[key] || defaultValue;
   if (value == null) {
-    throw new Error(`키${key}는 undefined!!`);
+    throw new Error(`키 ${key}는 undefined!!`);
   }
   return value;
 }
--- controller/auth.mjs
+++ controller/auth.mjs
@@ -4,41 +4,34 @@ import * as bcrypt from "bcrypt";
 import jwt from "jsonwebtoken";
 import { config } from "../config.mjs";
 
-const secretKey = "abcdefg1234!@#$";
-const bcryptSaltRounts = 10;
-const jwtExpiresInDays = "2d";
-
 async function createJwtToken(id) {
   return jwt.sign({ id }, config.jwt.secretKey, {
     expiresIn: config.jwt.expiresInSec,
   });
 }
 
-// 회원가입 함수
 export async function signup(req, res, next) {
   const { userid, password, name, email } = req.body;
 
-  //회원 중복 체크
+  // 회원 중복 체크
   const found = await authRepository.findByUserid(userid);
   if (found) {
-    return res.status(400).json({ message: `${userid}이 이미 있습니다` });
+    return res.status(409).json({ message: `${userid}이 이미 있습니다` });
   }
 
   const hashed = bcrypt.hashSync(password, config.bcrypt.saltRounds);
   const user = await authRepository.createUser(userid, hashed, name, email);
-
   //   const user = await authRepository.createUser(userid, password, name, email);
   const token = await createJwtToken(user.id);
   console.log(token);
   res.status(201).json({ token, user });
 }
 
-// 로그인 함수
 export async function login(req, res, next) {
   const { userid, password } = req.body;
   const user = await authRepository.findByUserid(userid);
   if (!user) {
-    res.status(401).json(`${userid}를 찾을 수 없음`);
+    res.status(401).json(`${userid} 를 찾을 수 없음`);
   }
   const isValidPassword = await bcrypt.compare(password, user.password);
   if (!isValidPassword) {
@@ -49,13 +42,11 @@ export async function login(req, res, next) {
   res.status(200).json({ token, user });
 }
 
-// 로그인 유지 함수
-
 export async function me(req, res, next) {
   //   const user = await authRepository.findByUserid(req.id);
   //   if (!user) {
-  //     return res.status(404).json({ message: "일치하는 사용자가 없습니다" });
+  //     return res.status(404).json({ message: "일치하는 사용자가 없음" });
   //   }
   //   res.status(200).json({ token: req.token, userid: user.userid });
-  res.status(200).json({ message: "성공했어~" });
+  res.status(200).json({ message: "성공했어 ~" });
 }
